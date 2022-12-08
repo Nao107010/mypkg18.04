b280d3fe583570d364bb565a34a55b39ef326180
Nao107010 <nao10701048@gmail.com>
2022-12-08 11:06:42 +0900

    Add a command

--- mypkg/talker.py
+++ mypkg/talker.py
@@ -1,13 +1,13 @@
 import rclpy
 from rclpy.node import Node
-from person_msgs.msg import Int16
+from std_msgs.msg import Int16
 
 rclpy.init()
 node = Node("talker")
 pub = node.create_publisher(Int16, "countup", 10)
 n = 0
 
-def cd():
+def cb():
     global n
     msg = Int16()
     msg.data = n
