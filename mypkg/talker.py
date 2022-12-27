ff0272fa507ac3b876456bcdbd4b8b8ba92bfba9
Nao107010 <nao10701048@gmail.com>
2022-12-28 00:47:44 +0900

    Add a command

--- mypkg/talker.py
+++ mypkg/talker.py
@@ -8,13 +8,13 @@ class Talker():
         self.n = 0
         node.create_timer(0.5, cb)
 
-def cb():
-    msg = Int16()
-    msg.data = talker.n
-    talker.pub.publish(msg)
-    talker.n += 1
+    def cb(self):
+        msg = Int16()
+        msg.data = self.n
+        self.pub.publish(msg)
+        self.n += 1
 
-rclpy.init()
-node = Node("talker")
-talker = Talker(node)
-rclpy.spin(node)
+    rclpy.init()
+    node = Node("talker")
+    talker = Talker(node)
+    rclpy.spin(node)
